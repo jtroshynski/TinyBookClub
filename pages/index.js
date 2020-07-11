9628d0927c96e942bacf25b38d4a9d99d966d94b
Jeremy Troshynski <troshyns@amazon.com>
2020-07-11 15:28:01 -0700

    Added dymanic page routing and two example markdown blog posts

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
 import utilStyles from '../styles/css/utils.module.css'
 
-export default function Home() {
+export default function Home({ allPostsData }){
   return (
     <Layout home>
       <Head>
@@ -14,6 +18,41 @@ export default function Home() {
           Welcome to Tiny Book Club, a two person one cat blog about books!
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
 }
+
+//fetches data at build time
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+//fetches data at request time
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       // TODO: fetch posts from database
+//     }
+//   }
+// }
